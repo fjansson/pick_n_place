d272c729ae740a73e3827ae880e1bdbade403481
Fredrik Jansson <fjansson@abo.fi>
2016-03-11 00:15:52 +0100

    hid joystick: rolling average, code cleanup

--- joystick/pro_micro_joystick_hid/pro_micro_joystick/pro_micro_joystick.ino
+++ joystick/pro_micro_joystick_hid/pro_micro_joystick/pro_micro_joystick.ino
@@ -6,8 +6,9 @@
  * hat switches: https://davenunez.wordpress.com/2014/02/22/reading-hat-switches-on-pc-gameport-joysticks/
  *               http://www.descent2.com/sickone/analogjoystick.html
  * 
- * Connections:
+ * linearization: http://www.built-to-spec.com/blog/2009/09/10/using-a-pc-joystick-with-the-arduino/
  * 
+ * Connections:
  * 
  *
  * Game port     Pro Micro
@@ -42,8 +43,8 @@
 
 #include "HID-Project.h"
 
-const int horzPin = A0;  // Analog output of horizontal joystick pin
-const int vertPin = A1;  // Analog output of vertical joystick pin
+const int xPin = A0;  // Analog output of horizontal joystick pin
+const int yPin = A1;  // Analog output of vertical joystick pin
 const int y2pin   = A2;  // Second y axis, used for hat.
 
 int button1 = 9;  
@@ -54,31 +55,27 @@ float x0f, y0f;  // scaled, linearized float values of center position
 
 int16_t x, y;    // Stores current analog output of each axis
 
-
-const int pinLed = LED_BUILTIN;
-const int pinButton = 9;
-
 const float Scale = 1023.0*55000; //1023 is the range of the ADC. 55000 is chosen for a nice range of output values.
+const float alpha = .1;  // constant for rolling average filter. smaller number -> slower filter.
 
+float x_filter = 0, y_filter = 0;
 
 void setup() 
 {
-  pinMode(horzPin, INPUT);  // Set both analog pins as inputs
-  pinMode(vertPin, INPUT);
+  pinMode(xPin, INPUT);  // Set both analog pins as inputs
+  pinMode(yPin, INPUT);
   pinMode(button1, INPUT);
   pinMode(button2, INPUT);
-  digitalWrite(button1, HIGH);  // Pull button select pin high
-  digitalWrite(button2, HIGH);  // Pull button select pin high
+  digitalWrite(button1, HIGH);  // Pull-ups on
+  digitalWrite(button2, HIGH);  
   
   delay(1000);  // short delay to let outputs settle
-  y0 = analogRead(vertPin);  // get the initial values
-  x0 = analogRead(horzPin);  // Joystick should be in neutral position when reading these
- 
+  
+  x0 = analogRead(xPin);  // get the initial values
+  y0 = analogRead(yPin);  // Joystick should be in neutral position when reading these
+  
   x0f = Scale/x0;
   y0f = Scale/y0;
-
-  pinMode(pinLed, OUTPUT);
-  pinMode(pinButton, INPUT_PULLUP);
 
   // Sends a clean report to the host. This is important on any Arduino type.
   Gamepad.begin();
@@ -86,10 +83,9 @@ void setup()
 
 void loop() 
 {
-
   // Analog axes 
-  y =  analogRead(vertPin);
-  x =  analogRead(horzPin);
+  x =  analogRead(xPin);
+  y =  analogRead(yPin);
 
   /* Simply return the raw ADC values, scaled.
   y = y - y0;
@@ -99,47 +95,31 @@ void loop()
   Gamepad.yAxis(y<<7);
   */
 
+  // convert to a linear scale
   float xf =  Scale/x - x0f;  
   float yf = -Scale/y + y0f;
-  
-  /*
-  Serial.print(x);
-  Serial.print(" ");
-  Serial.print(y);
-  Serial.print(" ");
-  Serial.print(xf);
-  Serial.print(" ");
-  Serial.print(yf);
-  Serial.print("\n"); */
 
-  Gamepad.xAxis((int)xf);
-  Gamepad.yAxis((int)yf);
+  // rolling average for filtering
+  x_filter = alpha * xf + (1-alpha) * x_filter;
+  y_filter = alpha * yf + (1-alpha) * y_filter;
+
+  Gamepad.xAxis((int)x_filter);
+  Gamepad.yAxis((int)y_filter);
   
   // Buttons
   uint32_t b=0;
   b |= !digitalRead(button1);
   b |= !digitalRead(button2) << 1;
-  
   Gamepad.buttons(b);
   
-  
+
   // Hat. Some (i.e. our) joystics encode the 4 hat switches with different analog values on axis y2.
   uint8_t dpad1 = GAMEPAD_DPAD_CENTERED;
   int hat = analogRead(y2pin);
   //Serial.print(hat);
   //Serial.print("\n");
 
-  // dir   observed ADC
-  // up    1014    threshold
-  //             926
-  // right  838
-  //             775
-  // down   712
-  //             667
-  // left   622
-  //             586
-  // center 550 
-
+  // the thresholds are half-way between the y2-values observed when using a 100K resistor for the voltage divider.
   if (hat > 775)
   {
     if (hat > 926)
@@ -154,50 +134,8 @@ void loop()
     else if (hat > 586)
       dpad1 = GAMEPAD_DPAD_LEFT;
   }
-  
   Gamepad.dPad1(dpad1); 
    
-    
   Gamepad.write();  
   delay(5);
-
-  
-  /*
-  if (!digitalRead(pinButton)) {
-    digitalWrite(pinLed, HIGH);
-
-    // Press button 1-32
-    static uint8_t count = 0;
-    count++;
-    if (count == 33) {
-      Gamepad.releaseAll();
-      count = 0;
-    }
-    else
-      Gamepad.press(count);
-
-    // Move x/y Axis to a new position (16bit)
-    Gamepad.xAxis(random(0xFFFF));
-    Gamepad.yAxis(random(0xFFFF));
-
-    // Go through all dPad positions
-    // values: 0-8 (0==centered)
-    static uint8_t dpad1 = GAMEPAD_DPAD_CENTERED;
-    Gamepad.dPad1(dpad1++);
-    if (dpad1 > GAMEPAD_DPAD_UP_LEFT)
-      dpad1 = GAMEPAD_DPAD_CENTERED;
-
-    static int8_t dpad2 = GAMEPAD_DPAD_CENTERED;
-    Gamepad.dPad2(dpad2--);
-    if (dpad2 < GAMEPAD_DPAD_CENTERED)
-      dpad2 = GAMEPAD_DPAD_UP_LEFT;
-
-    // Functions above only set the values.
-    // This writes the report to the host.
-    Gamepad.write();
-
-    // Simple debounce
-    delay(300);
-    digitalWrite(pinLed, LOW);
-  }*/
 }
